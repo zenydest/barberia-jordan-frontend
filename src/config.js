986792fe81fdddf5431bb1e16b96ea69923e03d6
zenydest <damiannieto51@gmail.com>
2026-01-10 21:36:28 -0300

    Fix CONFIG.JS

--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
-const API_URL = process.env.REACT_APP_API_URL || 'https://barberia-jordan-api.onrender.com';
-
+const API_URL = process.env.REACT_APP_API_URL || 'https://barberia-jordan-api.onrender.com/api';
 export default API_URL;
