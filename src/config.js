4f8c2de7668106e0ef0f75b9369673bdd718765c
zenydest <damiannieto51@gmail.com>
2026-01-12 12:10:35 -0300

    Fixed v1

--- src/config.js
+++ src/config.js
@@ -1,2 +1,6 @@
-const API_URL = process.env.REACT_APP_API_URL || 'https://barberia-jordan-api.onrender.com/api';
+const API_URL = import.meta.env.MODE === 'development' 
+  ? 'http://localhost:5000/api'
+  : 'https://barberia-jordan-api.onrender.com/api';
+
 export default API_URL;
+
