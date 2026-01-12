a30f3e0088d3d66e1e4af8766743fe0849bad7f9
zenydest <damiannieto51@gmail.com>
2026-01-12 12:21:09 -0300

    Fixed v2

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_URL = import.meta.env.MODE === 'development' 
   ? 'http://localhost:5000/api'
-  : 'https://barberia-jordan-api.onrender.com/api';
+  : 'https://web-production-ae8e1.up.railway.app/api';
 
 export default API_URL;
 
